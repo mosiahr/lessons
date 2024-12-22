392c2397f0e73f0ddcd8f684f32896bc6c4b9434
Gregory <greg.mosia@gmail.com>
2024-12-22 20:41:40 +0000

    L16: Create HTML structure of index.html

---
+++ lesson_16/js/script.js
@@ -0,0 +1,9 @@
+// burger-menu
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	// .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open 
+	if (targetElement.closest('.icon-menu')) {
+		document.documentElement.classList.toggle('open-menu');
+		e.preventDefault();
+	}
+})
